20e44cc378755e4212fe026c01be0d23ccf6e577
Betul Asena Celik <betulasenacelik@gmail.com>
2022-05-08 22:41:33 +0300

    first commit

---
+++ lib/screens/homepage.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:todo/widgets.dart';
+
+class Homepage extends StatefulWidget {
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+            width: double.infinity,
+            padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 32.0),
+            color: Color(0xFFF6F6F6),
+            child: Stack(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(bottom: 32),
+                      child: Image(
+                        width: 150,
+                        height: 150,
+                        image: AssetImage("assets/images/a.png"),
+                      ),
+                    ),
+                    TaskCardWidget(
+                      title: "Get Started",
+                      desc:
+                          "Hello User Welcome to App this is a default task that you can edit of delete to start using the app",
+                    ),
+                  ],
+                ),
+                Positioned(
+                  bottom: 0.0,
+                  right: 0.0,
+                  child: Container(
+                    decoration:
+                        BoxDecoration(color: Colors.deepPurpleAccent[700]),
+                    child: Image(image: AssetImage("assets/images/s.png")),
+                  ),
+                )
+              ],
+            )),
+      ),
+    );
+  }
+}
---
+++ lib/widgets.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class TaskCardWidget extends StatelessWidget {
+  final String title;
+  final String desc;
+  TaskCardWidget({required this.title, required this.desc});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.symmetric(
+        vertical: 32.0,
+        horizontal: 24.0,
+      ),
+      margin: EdgeInsets.only(bottom: 20.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(30.0),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title ?? "(Unnamed Task)",
+            style: TextStyle(
+              color: Colors.deepPurpleAccent[700],
+              fontSize: 22.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 10.0,
+            ),
+            child: Text(
+              desc ?? "(No desc)",
+              style: TextStyle(
+                fontSize: 16.0,
+                color: Colors.deepPurpleAccent[700],
+                height: 1.5,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
